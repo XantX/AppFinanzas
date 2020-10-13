4e4bae4b1ea932fda95a9b1cfee31eaa32a8826f
Sebastian <u201717471@upc.edu.pe>
2020-10-12 22:48:37 -0500

    Se anadio la barra de navegacion se agrego barra con bootstrap

--- App.py
+++ App.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def Landing():
-    return render_template('Landing.html') 
+    return render_template('Landing Page/Landing.html') 
 
+@app.route('/Home')
+def Home():
+    return 'Home'
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
