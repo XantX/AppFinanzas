893eede69741de47ef8fd6117dd6c45cad262d71
XantX <hp_xantx@hotmail.com>
2020-10-14 14:09:41 -0500

    se actualizo la barra de navegacion y carrucel se puso el color de fondo  la pagina y se agrego el carrucle con imagenes referentes a la eleccion de la divisa, el trato con le cliente y el control de los creditos

--- App.py
+++ App.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def Landing():
-    return render_template('Landing Page/Landing.html') 
+    return render_template('Landing.html') 
 
 @app.route('/Home')
 def Home():
     return 'Home'
+@app.route('/About')
+def About():
+    return 'About'
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
