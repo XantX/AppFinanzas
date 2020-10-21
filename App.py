1f24c9b6262914b844f9c1c9ef43b22feff4a399
XantX <hp_xantx@hotmail.com>
2020-10-21 10:36:44 -0500

    seccioon de about y login se  agrego las tarjetas para agregar los nombre y fotos de los desarroladores y se agrego la pagina de login

--- App.py
+++ App.py
@@ -12,5 +12,8 @@ def Home():
 @app.route('/About')
 def About():
     return 'About'
+@app.route('/login')
+def login():
+    return render_template('Login.html')
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
